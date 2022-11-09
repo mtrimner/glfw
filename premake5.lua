e15e041c33d50f6c615c77389a0f1e43c93b9a1d
mtrimner <67921571+mtrimner@users.noreply.github.com>
2022-11-08 18:39:58 -0600

    Updated submodule

--- premake5.lua
+++ premake5.lua
@@ -1,8 +1,6 @@
-
 project "GLFW"
 	kind "StaticLib"
 	language "C"
-	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -11,27 +9,32 @@ project "GLFW"
 	{
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
+		"src/internal.h",
+		"src/platform.h",
+		"src/mappings.h",
 		"src/context.c",
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
-
-		"src/null_init.c",
-		"src/null_joystick.c",
-		"src/null_monitor.c",
-		"src/null_window.c",
-
 		"src/platform.c",
 		"src/vulkan.c",
 		"src/window.c",
-	}
+		"src/egl_context.c",
+		"src/osmesa_context.c",
+		"src/null_platform.h",
+		"src/null_joystick.h",
+		"src/null_init.c",
 
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/null_joystick.c",
+	}
 	filter "system:linux"
 		pic "On"
 
 		systemversion "latest"
-		
+		staticruntime "On"
+
 		files
 		{
 			"src/x11_init.c",
@@ -53,14 +56,17 @@ project "GLFW"
 
 	filter "system:windows"
 		systemversion "latest"
+		staticruntime "On"
 
 		files
 		{
 			"src/win32_init.c",
-			"src/win32_joystick.c",
 			"src/win32_module.c",
+			"src/win32_joystick.c",
 			"src/win32_monitor.c",
+			"src/win32_time.h",
 			"src/win32_time.c",
+			"src/win32_thread.h",
 			"src/win32_thread.c",
 			"src/win32_window.c",
 			"src/wgl_context.c",
@@ -74,11 +80,6 @@ project "GLFW"
 			"_CRT_SECURE_NO_WARNINGS"
 		}
 
-		links
-		{
-			"Dwmapi.lib"
-		}
-
 	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
@@ -86,8 +87,3 @@ project "GLFW"
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
-
-	filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
-        symbols "off"
